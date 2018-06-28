c89d1918d1110292c223e6d1bbc365f7c5f4ed45
Matt Winzer <matt.winzer@gmail.com>
2018-06-28 13:13:58 -0600

    added cors module

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var gifsRouter = require('./routes/gifs');
+const indexRouter = require('./routes/index');
+const gifsRouter = require('./routes/gifs');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
 app.use('/gifs', gifsRouter);
