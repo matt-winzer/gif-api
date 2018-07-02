33489543af9783ed7f70c77da9b7cad07cc3103e
Matt Winzer <matt.winzer@gmail.com>
2018-07-02 12:53:06 -0600

    full CRUD routes for gifs

--- routes/gifs.js
+++ routes/gifs.js
@@ -1,12 +1,56 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const knex = require('../db/connection') 
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   knex('gif')
     .then(gifs => {
       res.json(gifs)
     })
+    .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+  knex('gif')
+    .where('id', req.params.id)
+    .then(gif => {
+      !gif.length && res.json({message: 'Gif not found. Please try another id.'})
+      res.json(gif[0])
+    })
+    .catch(next)
+})
+
+router.put('/:id', (req, res, next) => {
+  knex('gif')
+    .where('id', req.params.id)
+    .update(req.body)
+    .returning('*')
+    .then(updated => {
+      res.json({updated: updated[0]})
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  knex('gif')
+    .insert(req.body)
+    .returning('*')
+    .then(inserted => {
+      res.json({inserted: inserted[0]})
+    })
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  knex('gif')
+    .where('id', req.params.id)
+    .delete()
+    .returning('*')
+    .then(deleted => {
+      !deleted.length && res.json({message: 'Gif not found. Please try another id.'})
+      res.json({deleted: deleted[0]})
+    })
+    .catch(next)
 })
 
 module.exports = router
